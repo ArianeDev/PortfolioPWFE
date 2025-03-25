05f9b990bccb2f833f5b18ea5424291034788b1c
Ariane Silva <silvaarianeoliveira2@gmail.com>
2025-03-25 09:47:56 -0300

    Continuação

--- JS/ListaExercicio/Exercicio04/JS/exercicios.js
+++ JS/ListaExercicio/Exercicio04/JS/exercicios.js
@@ -22,12 +22,58 @@ let media = soma / (notas.length);
 
 resposta02.innerHTML = `<p>O valor da média é ${media.toFixed(2)}</p>`;
 
+// Exercício 03 
+const resposta03 = document.getElementById('resposta03');
+let idades = [];
+
+function Adicionar(){
+    let idade = parseInt(document.getElementById('idade').value);
+    
+    if(!isNaN(idade)){
+        idades.push(idade);
+        console.log(idades);
+
+        atualizarLista();
+        idade = document.getElementById('idade').value = '';
+
+        if(numeros.length === 20){
+            alert("oi")
+        }
+    }
+}
+
 // Exercício 05
-const resposta05 = document.getElementById('responsta05');
+const resposta05 = document.getElementById('resposta05');
+const form05 = document.getElementById('form05');
 
 const nomes = ["Joyce", "Ariane", "Nicolas", "Thifany", "João", "Gabriela", "Geovanna", "Resende", "Evellyn", "Lívia"];
-const nome = document.getElementById('nome');
+let text = '';
 
-for(nome in nomes){
-    resposta05.innerHTML= `<p>O indice do nome é</p>`
+form05.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    let nome = document.getElementById('nome').value;
+
+    for(let i in nomes){
+        if(nomes[i] === nome){
+            resposta05.innerHTML = `<p>O indice do nome é ${i}</p>`;
+            break;
+        } else {
+            resposta05.innerHTML = `<p>Nome não encontrado</p>`;
+        }
+    }
+})
+
+// Exercício 07
+const input = document.getElementById('exercicio07');
+const resposta07 = document.getElementById('resposta07');
+
+const btn_add = document.getElementById('btn_add');
+
+function InputNomes(){
+    
+
+
+
 }
+
